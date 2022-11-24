ee61c9d3be05874afd2e2f7b2d249748453230e0
Harold Pulcher <pulcher@killercomputing.com>
2022-11-23 18:28:42 -0600

    working on some test type code to figure out what is going on

---
+++ exploration/dynamicDNSTest/dynamicDNSTest/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace dynamicDNSTest
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string hostName = Dns.GetHostName();
+
+            DoGetHostAddresses(hostName);
+
+            DisplayDnsConfiguration();
+        }
+
+        public static void DoGetHostAddresses(string hostname)
+        {
+            var addresses = Dns.GetHostAddresses(hostname);
+
+            Console.WriteLine($"GetHostAddresses({hostname}) returns:");
+
+            foreach (IPAddress address in addresses)
+            {
+                Console.WriteLine($"    {address}");
+            }
+        }
+
+        public static void DisplayDnsConfiguration()
+        {
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface adapter in adapters)
+            {
+                IPInterfaceProperties properties = adapter.GetIPProperties();
+                var ipAddresses = properties.DnsAddresses.ToList();
+
+                Console.WriteLine(adapter.Description);
+                Console.WriteLine("  DNS suffix .............................. : {0}",
+                    properties.DnsSuffix);
+                Console.WriteLine("  DNS enabled ............................. : {0}",
+                    properties.IsDnsEnabled);
+                Console.WriteLine("  Dynamically configured DNS .............. : {0}",
+                    properties.IsDynamicDnsEnabled);
+                Console.WriteLine("  IP Address .............. : {0}",
+                 properties.DnsAddresses.Select(a => a.AddressFamily.GetTypeCode ));
+            }
+            Console.WriteLine();
+        }
+    }
+}
